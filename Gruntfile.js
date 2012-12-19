404f7aa21150e59534d4eef9d817480094013a5e
Mike Cravey <techie@craveytrain.com>
2012-12-19 00:18:39 -0600

    Dynamically rendering jade config from render

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,16 @@
+/*jshint quotmark:false */
 module.exports = function(grunt) {
+'use strict';
 
 // Project configuration.
 grunt.initConfig({
   pkg: grunt.file.readJSON('package.json'),
+  render: {
+	stuff: 'yeah'
+  },
   jade: {
-	compile: {
-		options: {
-			data: {
-				page: {
-					title: 'test title',
-					description: 'test description',
-					author: 'Mike Cravey',
-					canonical: 'http://craveytrain.com/path/to/page',
-					pageSlug: 'home',
-					pageType: 'home',
-					body: '<div>foo</div>'
-				},
-				site: {
-					title: 'craveytrain',
-					subtitle: "I'm a little geek bot, short and stout…",
-					rss: '/atom.xml',
-					stylesheet: '/styles.css',
-					domain: 'craveytrain.com'
-				}
-			},
-			pretty: true
-		},
-		files: {
-			'build/index.html': 'source/layout.jade'
-		}
+	options: {
+		pretty: true
 	}
   },
   server: {
@@ -40,8 +22,9 @@ grunt.initConfig({
 
 grunt.loadNpmTasks('grunt-contrib-jade');
 grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadTasks('lib');
 
-grunt.registerTask('build', ['clean', 'jade']);
+grunt.registerTask('build', ['clean', 'render']);
 
 // Default task(s).
 grunt.registerTask('default', ['build']);
