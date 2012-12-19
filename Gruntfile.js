a1fe7fedc2eca908bd3aeed2d0cc20afc7b30073
Mike Cravey <techie@craveytrain.com>
2012-12-19 14:43:29 -0600

    Adds local server

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,27 +4,35 @@ module.exports = function(grunt) {
 
 // Project configuration.
 grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
-  render: {
-	stuff: 'yeah'
-  },
-  jade: {
-	options: {
-		pretty: true
-	}
-  },
-  server: {
-	port: 4000,
-	base: './build'
-  },
-  clean: ['build']
+    pkg: grunt.file.readJSON('package.json'),
+    render: {
+        stuff: 'yeah'
+    },
+    jade: {
+        options: {
+           pretty: true
+        }
+    },
+    connect: {
+        server: {
+            options: {
+                port: 4000,
+                base: 'build',
+                keepalive: true
+            }
+        }
+    },
+    clean: ['build']
 });
 
 grunt.loadNpmTasks('grunt-contrib-jade');
 grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-connect');
 grunt.loadTasks('lib');
 
 grunt.registerTask('build', ['clean', 'render']);
+// Preview the site
+grunt.registerTask('preview', ['build', 'connect:server']);
 
 // Default task(s).
 grunt.registerTask('default', ['build']);
