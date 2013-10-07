c0ac218a9165112e22b69ed5fb076734f6e4204b
Mike Cravey <techie@craveytrain.com>
2013-10-06 21:21:47 -0500

    Got JS source maps working, finally.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,120 +3,136 @@ module.exports = function(grunt) {
 
 // Project configuration.
 grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	pkg: grunt.file.readJSON('package.json'),
 
-    copy: {
-        dist: {
-            files: [
-                {
-                    cwd: 'source/',
-                    expand: true,
-                    src: ['**', '!stylesheets/**'],
-                    dest: 'build/'
-                }
-            ]
-        }
-    },
+	copy: {
+		dist: {
+			files: [
+				{
+					cwd: 'source/',
+					expand: true,
+					src: ['**', '!stylesheets/**'],
+					dest: 'build/'
+				}
+			]
+		}
+	},
 
-    render: {
-        content: ['content/**/*.md'],
-        target: 'build',
-        templates: 'templates',
-    },
+	render: {
+		content: ['content/**/*.md'],
+		target: 'build',
+		templates: 'templates',
+	},
 
-    jade: {
-        options: {
-           pretty: true
-        }
-    },
+	jade: {
+		options: {
+			pretty: true
+		}
+	},
 
-    stylus: {
-        compile: {
-            options: {
-                linenos: true
-            },
-            files: {
-                'build/css/default.css': 'source/stylesheets/default.styl'
-            }
-        }
-    },
+	stylus: {
+		compile: {
+			options: {
+				linenos: true
+			},
+			files: {
+				'build/css/default.css': 'source/stylesheets/default.styl'
+			}
+		}
+	},
 
-    uglify: {
-        dist: {
-            options: {
-                sourceMap: 'build/scripts/main.min.js.map',
-                sourceMappingURL: 'main.min.js.map',
-                prefix: '2'
-            },
-            files: {
-                'build/scripts/main.min.js': 'build/scripts/main.js'
-            }
-        }
-    },
+	uglify: {
+		dist: {
+			options: {
+				report: 'gzip',
+				sourceMapRoot: '/',
+				sourceMap: 'build/js/source-map.js',
+				sourceMapPrefix: 1,
+				sourceMappingURL: '/js/source-map.js'
+			},
+			files: [
+				{
+					expand: true,		// Enable dynamic expansion.
+					cwd: 'build/js/',	// Src matches are relative to this path.
+					src: ['**/*.js'],	// Actual pattern(s) to match.
+					dest: 'build/js/',	// Destination path prefix.
+					ext: '.min.js'		// Dest filepaths will have this extension.
+				}
+			]
+		}
+	},
 
-    watch: {
-        css: {
-            files: 'source/stylesheets/**/*.styl',
-            tasks: ['stylus'],
-            options: {
-                interrupt: true
-            }
-        },
+	watch: {
+		css: {
+			files: 'source/stylesheets/**/*.styl',
+			tasks: ['stylus'],
+			options: {
+				interrupt: true
+			}
+		},
 
-        templates: {
-            files: ['**/*.yml', 'templates/**/*.jade', 'content/**/*.md'],
-            tasks: ['render'],
-            options: {
-                interrupt: true
-            }
-        },
+		js: {
+			files: 'source/js/**/*.js',
+			tasks: ['uglify'],
+			options: {
+				interrupt: true
+			}
+		},
 
-        staticFiles: {
-            files: ['source/**', '!source/stylesheets/**'],
-            tasks: ['copy'],
-            options: {
-                interrupt: true
-            }
-        }
-    },
+		templates: {
+			files: ['**/*.yml', 'templates/**/*.jade', 'content/**/*.md'],
+			tasks: ['render'],
+			options: {
+				interrupt: true
+			}
+		},
 
-    jshint: {
-        options: {
-            jshintrc: '.jshintrc'
-        },
-        grunt: ['Gruntfile.js', 'lib/**/*.js', 'tasks/**/*.js'],
-        site: ['source/**/*.js']
+		staticFiles: {
+			files: ['source/**', '!source/stylesheets/**'],
+			tasks: ['copy'],
+			options: {
+				interrupt: true
+			}
+		}
+	},
 
-    },
+	jshint: {
+		options: {
+			jshintrc: '.jshintrc'
+		},
+		grunt: ['Gruntfile.js', 'lib/**/*.js', 'tasks/**/*.js'],
+		site: ['source/**/*.js']
 
-    htmlmin: {
-        dist: {
-            options: {
-                removeComments: true,
-                removeCommentsFromCDATA: true,
-                removeCDATASectionsFromCDATA: true,
-                collapseWhitespace: true,
-                collapseBooleanAttributes: true,
-                removeRedundantAttributes: true,
-                removeEmptyAttributes: true,
-                removeOptionalTags: true
-            },
-            files: {
-                'build/**/*.html': 'build/**/*.html'
-            }
-        }
-    },
+	},
 
-    connect: {
-        server: {
-            options: {
-                port: 4000,
-                base: 'build'
-            }
-        }
-    },
+	htmlmin: {
+		dist: {
+			options: {
+				removeComments: true,
+				removeCommentsFromCDATA: true,
+				removeCDATASectionsFromCDATA: true,
+				collapseWhitespace: true,
+				collapseBooleanAttributes: true,
+				removeRedundantAttributes: true,
+				removeEmptyAttributes: true,
+				removeOptionalTags: true
+			},
+			files: {
+				'build/**/*.html': 'build/**/*.html'
+			}
+		}
+	},
 
-    clean: ['build']
+	connect: {
+		server: {
+			options: {
+				port: 4000,
+				base: 'build'
+			}
+		}
+	},
+
+	clean: ['build']
 });
 
 grunt.loadNpmTasks('grunt-contrib-jade');
@@ -130,9 +146,9 @@ grunt.loadNpmTasks('grunt-contrib-htmlmin');
 grunt.loadNpmTasks('grunt-contrib-stylus');
 grunt.loadTasks('tasks');
 
-grunt.registerTask('build', ['clean', 'jshint', 'copy', 'render', 'stylus']);
+grunt.registerTask('build', ['clean', 'jshint', 'copy', 'render', 'stylus', 'uglify']);
 grunt.registerTask('preview', ['build', 'connect', 'watch']);
-grunt.registerTask('package', ['clean', 'jshint', 'copy', 'render', 'stylus', 'uglify']);
+// grunt.registerTask('package', ['clean', 'jshint', 'copy', 'render', 'stylus', 'uglify']);
 
 // Default task(s).
 grunt.registerTask('default', ['build']);
