d9e5bf5537a39d7a8ab5d9569191eb8c32a3cb96
Mike Cravey <techie@craveytrain.com>
2016-07-15 15:51:35 +0100

    Stashing gistify

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@
 // Gulp and related plugins
 var gulp = require( 'gulp' );
 var util = require( 'gulp-util' );
-var sass = require( 'gulp-sass' );
-var autoprefixer = require( 'gulp-autoprefixer' );
+// var sass = require( 'gulp-sass' );
+// var autoprefixer = require( 'gulp-autoprefixer' );
 var connect = require( 'gulp-connect' );
 var del = require( 'del' );
-var sourcemaps = require( 'gulp-sourcemaps' );
-var uglify = require( 'gulp-uglify' );
+// var sourcemaps = require( 'gulp-sourcemaps' );
+// var uglify = require( 'gulp-uglify' );
 var markdown = require( 'gulp-markdown-to-json' );
-var jshint = require( 'gulp-jshint' );
-var stylish = require( 'jshint-stylish' );
-var jscs = require( 'gulp-jscs' );
+// var jshint = require( 'gulp-jshint' );
+// var stylish = require( 'jshint-stylish' );
+// var jscs = require( 'gulp-jscs' );
 
 // Home grown modules
 var excerpt = require( './tasks/excerpt' );
@@ -34,47 +34,47 @@ gulp.task( 'clean', function ( cb ) {
 	del( [ buildDir ], cb );
 } );
 
-gulp.task( 'checkJs', function () {
-	return gulp.src( [
-			'**/*.js',
-			'!node_modules/**/*.js',
-			'!' + buildDir + '/**/*.js'
-		] )
-		.pipe( jshint() )
-		.pipe( jshint.reporter( stylish ) )
-		.pipe( jscs() );
-} );
-
-gulp.task( 'css', function () {
-	return gulp.src( 'sass/screen.scss' )
-		.pipe( sourcemaps.init() )
-		.pipe( sass() )
-		.pipe( autoprefixer( 'last 2 version' ) )
-		.pipe( sourcemaps.write( './maps' ) )
-		.pipe( gulp.dest( buildDir + '/css' ) )
-		.pipe( reload( {
-			stream: true
-		} ) );
-} );
-
-gulp.task( 'js', function () {
-	return gulp.src( 'js/main.js' )
-		.pipe( sourcemaps.init() )
-		.pipe( uglify() )
-		.pipe( sourcemaps.write( './maps' ) )
-		.pipe( gulp.dest( buildDir + '/js' ) )
-		.pipe( reload( {
-			stream: true
-		} ) );
-} );
-
-gulp.task( 'copy', function () {
-	return gulp.src( sourceGlob )
-		.pipe( gulp.dest( buildDir ) )
-		.pipe( reload( {
-			stream: true
-		} ) );
-} );
+// gulp.task( 'checkJs', function () {
+// 	return gulp.src( [
+// 			'**/*.js',
+// 			'!node_modules/**/*.js',
+// 			'!' + buildDir + '/**/*.js'
+// 		] )
+// 		.pipe( jshint() )
+// 		.pipe( jshint.reporter( stylish ) )
+// 		.pipe( jscs() );
+// } );
+//
+// gulp.task( 'css', function () {
+// 	return gulp.src( 'sass/screen.scss' )
+// 		.pipe( sourcemaps.init() )
+// 		.pipe( sass() )
+// 		.pipe( autoprefixer( 'last 2 version' ) )
+// 		.pipe( sourcemaps.write( './maps' ) )
+// 		.pipe( gulp.dest( buildDir + '/css' ) )
+// 		.pipe( reload( {
+// 			stream: true
+// 		} ) );
+// } );
+//
+// gulp.task( 'js', function () {
+// 	return gulp.src( 'js/main.js' )
+// 		.pipe( sourcemaps.init() )
+// 		.pipe( uglify() )
+// 		.pipe( sourcemaps.write( './maps' ) )
+// 		.pipe( gulp.dest( buildDir + '/js' ) )
+// 		.pipe( reload( {
+// 			stream: true
+// 		} ) );
+// } );
+//
+// gulp.task( 'copy', function () {
+// 	return gulp.src( sourceGlob )
+// 		.pipe( gulp.dest( buildDir ) )
+// 		.pipe( reload( {
+// 			stream: true
+// 		} ) );
+// } );
 
 gulp.task( 'render', function () {
 	return gulp.src( mdGlob )
